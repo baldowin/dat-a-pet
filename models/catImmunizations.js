1f46e7b21423b3169bb265e36a5330fcfe6370c1
JBloodBeardN <jtn.neese@gmail.com>
2019-04-16 14:31:26 -0500

    catImmunzation stash

--- models/catImmunizations.js
+++ models/catImmunizations.js
@@ -2,37 +2,37 @@ module.exports = function (sequelize, DataTypes) {
 
   var catImmunizations = sequelize.define("catImmunizations", {
 
-    FVRCP_6_8_weeks:
+    FVRCP_6_8_Weeks:
         {
           type: DataTypes.INTEGER,
           allowNull: false,
           defaultValue: 2
         },
-    FVRCP_9_11_weeks:
+    FVRCP_9_11_Weeks:
         {
           type: DataTypes.INTEGER,
           allowNull: false,
           defaultValue: 3
         },
-    FELV_9_11_weeks:
+    FELV_9_11_Weeks:
         {
           type: DataTypes.INTEGER,
           allowNull: false,
           defaultValue: 3
         },
-    FVRCP__16_weeks:
+    FVRCP__16_Weeks:
         {
           type: DataTypes.INTEGER,
           allowNull: false,
           defaultValue: 5
         },
-    FELV__16_weeks:
+    FELV__16_Weeks:
         {
           type: DataTypes.INTEGER,
           allowNull: false,
           defaultValue: 4
         },
-    RABIES__16_weeks:
+    RABIES__16_Weeks:
         {
           type: DataTypes.INTEGER,
           allowNull: false,
